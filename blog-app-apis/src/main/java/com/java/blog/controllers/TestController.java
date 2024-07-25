b138f7adbd40d158ed44cb0cabba78dc3561d11d
nikhil-duryan <nikhilkumarduryan11@gmail.com>
2024-07-26 01:14:14 +0530

    Added /greet api

--- blog-app-apis/src/main/java/com/java/blog/controllers/TestController.java
+++ blog-app-apis/src/main/java/com/java/blog/controllers/TestController.java
@@ -1,12 +1,23 @@
 package com.java.blog.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.java.blog.config.Test;
+
 @RestController
 public class TestController {
+
+    @Autowired
+    private Test test;
+
     @GetMapping("/hello")
     public String hello() {
         return "Welcome";
-    }   
+    }
+    @GetMapping("/greet")
+    public String greet() {
+        return test.greet();
+    }
 }
